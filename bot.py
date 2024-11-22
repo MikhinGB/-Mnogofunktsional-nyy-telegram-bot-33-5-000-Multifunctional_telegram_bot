790eec1c5c0751c17a97b576e7db318aa9f45942
Григорий Михин <161627435+MikhinGB@users.noreply.github.com>
2024-11-22 21:16:20 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-from PIL import Image
+from PIL import Image, ImageOps
 import io
 from telebot import types
 import os
@@ -14,17 +14,34 @@ ASCII_CHARS = ''
 
 
 def resize_image(image, new_width=100):
+
+    """ Изменяет размер изображения с сохранением пропорций."""
+
     width, height = image.size
     ratio = height / width
     new_height = int(new_width * ratio)
     return image.resize((new_width, new_height))
 
+def invert_colors(image):
+
+    """ Преобразует изображение в инверсионное (эффект негатива) """
+
+    im_invert = ImageOps.invert(image)
+    return im_invert
+
 
 def grayify(image):
+
+    """ Преобразует цветное изображение в оттенки серого. """
+
     return image.convert("L")
 
 
 def image_to_ascii(image_stream, new_width=40):
+
+    """ Основная функция для преобразования изображения в ASCII-арт. Изменяет размер, преобразует в градации серого
+    и затем в строку ASCII-символов."""
+
     # Переводим в оттенки серого
     image = Image.open(image_stream).convert('L')
 
@@ -49,6 +66,10 @@ def image_to_ascii(image_stream, new_width=40):
 
 
 def pixels_to_ascii(image):
+
+    """ Конвертирует пиксели изображения в градациях серого в строку ASCII-символов, используя предопределенную строку
+    ASCII_CHARS"""
+
     pixels = image.getdata()
     characters = ""
     for pixel in pixels:
@@ -58,23 +79,34 @@ def pixels_to_ascii(image):
 
 # Огрубляем изображение
 def pixelate_image(image, pixel_size):
+
+    """ Принимает изображение и размер пикселя. Уменьшает изображение до размера, где один пиксель представляет большую
+     область, затем увеличивает обратно, создавая пиксельный эффект."""
+
     image = image.resize(
         (image.size[0] // pixel_size, image.size[1] // pixel_size),
-        Image.NEAREST
+        Image.Resampling.NEAREST
     )
     image = image.resize(
         (image.size[0] * pixel_size, image.size[1] * pixel_size),
-        Image.NEAREST
+        Image.Resampling.NEAREST
     )
     return image
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+
+    """ Обработчик сообщений, реагирует на команды /start и /help, отправляя приветственное сообщение. """
+
     bot.reply_to(message, "Send me an image, and I'll provide options for you!")
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
+
+    """ Обработчик сообщений, реагирует на изображения, отправляемые пользователем. Предлагает
+    пользователю ввести уникальный набор символов ASCII и выбрать варианты обработки."""
+
     bot.reply_to(message, "I got your photo! Please choose what you'd like to do with it.",
                  reply_markup=get_options_keyboard())
     user_states[message.chat.id] = {'photo': message.photo[-1].file_id}
@@ -83,30 +115,48 @@ def handle_photo(message):
 
 @bot.message_handler(content_types=['text'])
 def save_ascii_chars(message):
+
+    """ Обработчик сообщений, принимает уникальный набор символов ASCII, введенный пользователем."""
+
     global ASCII_CHARS
     ASCII_CHARS = message.text
     bot.reply_to(message, f'Ваши данные успешно сохранены!')
 
 
 def get_options_keyboard():
+
+    """ Создает клавиатуру с кнопками для выбора пользователем, как обработать изображение: через пикселизацию или
+    преобразование в ASCII-арт. """
+
     keyboard = types.InlineKeyboardMarkup()
     pixelate_btn = types.InlineKeyboardButton("Pixelate", callback_data="pixelate")
     ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
-    keyboard.add(pixelate_btn, ascii_btn)
+    negative_btn = types.InlineKeyboardButton("Negative", callback_data="negative")
+    keyboard.add(pixelate_btn, ascii_btn, negative_btn)
     return keyboard
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
+
+    """ Определяет действия в ответ на выбор пользователя (например, пикселизация или ASCII-арт) и вызывает
+    соответствующую функцию обработки.
+"""
     if call.data == "pixelate":
         bot.answer_callback_query(call.id, "Pixelating your image...")
         pixelate_and_send(call.message)
     elif call.data == "ascii":
         bot.answer_callback_query(call.id, "Converting your image to ASCII art...")
         ascii_and_send(call.message)
+    elif call.data == "negative":
+        bot.answer_callback_query(call.id, "Creating a negative image...")
+        invert_and_send(call.message)
 
 
 def pixelate_and_send(message):
+
+    """ Пикселизирует изображение и отправляет его обратно пользователю."""
+
     photo_id = user_states[message.chat.id]['photo']
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -122,6 +172,8 @@ def pixelate_and_send(message):
 
 
 def ascii_and_send(message):
+
+    """ Преобразует изображение в ASCII-арт и отправляет результат в виде текстового сообщения."""
     photo_id = user_states[message.chat.id]['photo']
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -129,6 +181,23 @@ def ascii_and_send(message):
     image_stream = io.BytesIO(downloaded_file)
     ascii_art = image_to_ascii(image_stream)
     bot.send_message(message.chat.id, f"```\n{ascii_art}\n```", parse_mode="MarkdownV2")
+
+
+def invert_and_send(message):
+
+    """ Преобразует изображение в 'негатив' и  отправляет его обратно пользователю. """
+    photo_id = user_states[message.chat.id]['photo']
+    file_info = bot.get_file(photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_stream = io.BytesIO(downloaded_file)
+    image = Image.open(image_stream)
+    inverted = invert_colors(image)
+
+    output_stream = io.BytesIO()
+    inverted.save(output_stream, format="JPEG")
+    output_stream.seek(0)
+    bot.send_photo(message.chat.id, output_stream)
 
 
 bot.polling(none_stop=True)
